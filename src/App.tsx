218e1960a0db556d93214f29044d53d9b98da154
Dennis Paixão <dennispaixaoads@gmail.com>
2025-05-25 17:57:08 -0300

    solution2

--- src/App.tsx
+++ src/App.tsx
@@ -15,74 +15,105 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  None = '',
+  Alphabet = 'alphabet',
+  Length = 'length',
+}
+
+interface SortOptions {
+  sortField: SortType;
+  isReversed: boolean;
+}
+
+function sortGoodsBy(
+  goods: string[],
+  { sortField, isReversed }: SortOptions,
+): string[] {
+  const sortedGoods = [...goods];
+
+  if (sortField) {
+    switch (sortField) {
+      case SortType.Alphabet:
+        sortedGoods.sort((good1, good2) => good1.localeCompare(good2));
+        break;
+
+      case SortType.Length:
+        sortedGoods.sort((good1, good2) => good1.length - good2.length);
+        break;
+
+      default:
+        throw new Error('Parameter is not correct!');
+    }
+  }
+
+  if (isReversed) {
+    sortedGoods.reverse();
+  }
+
+  return sortedGoods;
+}
+
 export const App: React.FC = () => {
-  // Estado para gerenciar a lista atual
-  const [goods, setGoods] = useState(goodsFromServer);
+  const [sortField, setSortField] = useState<SortType>(SortType.None);
+  const [isReversed, setIsReversed] = useState<boolean>(false);
 
-  const sortAlphabetically = () => {
-    const sorted = [...goods].sort((a, b) => a.localeCompare(b));
-
-    setGoods(sorted);
-  };
-
-  const sortByLength = () => {
-    const sorted = [...goods].sort((a, b) => a.length - b.length);
-
-    setGoods(sorted);
-  };
-
-  // CORREÇÃO CRÍTICA: sempre reverte a lista original
-  const reverse = () => {
-    const reversed = [...goods].reverse();
-
-    setGoods(reversed);
-  };
-
-  const reset = () => {
-    setGoods([...goodsFromServer]);
-  };
+  const renderingGoods: string[] = sortGoodsBy(goodsFromServer, {
+    sortField,
+    isReversed,
+  });
 
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
-          onClick={sortAlphabetically}
+          className={`button is-info ${sortField === SortType.Alphabet ? '' : 'is-light'}`}
+          onClick={() => {
+            setSortField(SortType.Alphabet);
+          }}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
-          onClick={sortByLength}
+          className={`button is-success ${sortField === SortType.Length ? '' : 'is-light'}`}
+          onClick={() => {
+            setSortField(SortType.Length);
+          }}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
-          onClick={reverse}
+          className={`button is-warning ${isReversed ? '' : 'is-light'}`}
+          onClick={() => {
+            setIsReversed(!isReversed);
+          }}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-          onClick={reset}
-        >
-          Reset
-        </button>
+        {(sortField !== SortType.None || isReversed) && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setSortField(SortType.None);
+              setIsReversed(false);
+            }}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
-      {/* CORREÇÃO: estrutura HTML válida e lista dinâmica */}
       <ul>
-        {goods.map((item, index) => (
-          <li key={`${item}-${index}`} data-cy="Good">
-            {item}
+        {renderingGoods.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
           </li>
         ))}
       </ul>
